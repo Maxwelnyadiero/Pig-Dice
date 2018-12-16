100aff921aadf0488605e7e82c14b874a031c2e9
rosemarymagoma99@gmail.com <maxwelnyadiero@gmail.com>
2018-12-16 21:05:03 +0300

    added functions

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+var Opponent = function(name, score, status) {
+  this.name = name;
+  this.score = score;
+  this.status = status;
+}
+opponent.prototype.scoreplus = function () {
+  this.score += grandtotal;
+
+}
