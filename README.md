6267f64b8a3a6b3df1f4ee64a58dbba429833869
rosemarymagoma99@gmail.com <maxwelnyadiero@gmail.com>
2018-12-17 08:42:18 +0300

    styled h2

--- README.md
+++ README.md
@@ -7,16 +7,11 @@ The game must involve two people or parties whereby if the first party is playin
 ## Link to live site on GitHub pages:
 https://github.com/Maxwelnyadiero/Pig-Dice.git
 ## Behavior Driven Development:
-|behavior              |input      |output           |
-|:---------------------|:----------|:----------------|
-|                      |           |                 |
-|                      |           |                 |
-|                      |           |                 |
+The two parties are to agree on who should go first,that is the opponents.after coming into conclusion,the firstparty starts the game by rolling the dice,the number of times the dice rolls is equivalent to the number of points by which he or she will achieve.but when the die only roll once all the points by which he had managed to achieve are all destroyed,hence the second party takes on to the game,the second party also begins the game and all conditions remains the same,the first party to reach the winning point becomes the winner of the game.
 ## Technology used:
 1. JavaScript
 2. CSS
 3. HTML
 4. jQuery
 5. Bootstrap
-6. git
 ## Copyright and license information:
--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,74 @@ var Opponent = function(name, score, status) {
   this.score = score;
   this.status = status;
 }
-opponent.prototype.scoreplus = function () {
+opponent.prototype.scoreplus = function() {
   this.score += grandtotal;
-
 }
+var firstopponent();
+var secondopponent();
+var roll = 0;
+var grandTotal = 0;
+var roll = function() {
+  roll = math.floor(math.random() * 6) + 1;
+  jQuery("#new-dice").remove();
+  if (roll != 1) {
+    grandTotal += roll
+  } else {
+    grandTotal = 0;
+    turnOpponent();
+  }
+}
+var turnOpponent = function() {
+  if (firstOpponent.status == "enable") {
+    firstOpponent.status = "disable";
+    secondOpponent.status = "enable";
+    jQuery(".one-turn").show();
+    jQuery(".two-turn").hide();
+  } else if (secondOpponent.status == "enable") {
+    firstOpponent.status = "enable";
+    secondOpponent.status = "disable";
+    jQuery(".secondOpponent").show();
+    jQuery(".secondOpponent").hide();
+  }
+}
+var winner = function() {
+  if (playerOne.score >= 100) {
+    jQuery(".dice-img").text("!!!" + firstOpponent.name + "  WINS !!!")
+    swal({
+      title: firstOpponent.name + "  WINS !!!",
+      icon: "success",
+      button: "OK!",
+    });
+    jQuery("#roll").prop("disable", true);
+    jQuery("#hold").prop("disable", true);
+  } else if (secondOpponent.score >= 100) {
+    jQuery(".dice-img").text("!!!" + secondOpponent.name + "  WINS !!!")
+    swal({
+      title: secondOpponent.name + "  WINS !!!",
+      icon: "success",
+      button: "OK!",
+    });
+    jQuery("#roll").prop("disabled", true);
+    jQuery("#hold").prop("disabled", true);
+  }
+}
+
+jQuery(document).ready(function() {
+      jQuery(".opponnents").submit(function(event) {
+            event.preventDefault();
+            firstOpponentName = $("#player-one").val();
+            secondOpponentName = $("#player-two").val();
+            secondOpponent = new Player(secondOpponentName, 0, "disable");
+            firstOpponent = new Player(firstOpponentName, 0, "enable");
+
+            jQuery(".login").hide();
+            jQuery(".pig-dice").show();
+            jQuery(".firstOpponentname").prepend(firstOpponent.name);
+            jQuery(".secondOpponentname").prepend(secondOpponent.name);
+            jQuery(".two-turn").show();
+            jQuery(".one-turn").hide();
+
+            jQuery("#roll").click(function() {
+              roll();
+              jQuery("#grandTotal").text(grandTotal);
+            })
